29d51921698b4f4e05fcac7282f4011e09dbbeac
Rangga <leorangga30@ymail.com>
2021-01-06 11:42:13 +0700

    initial commit

---
+++ klikfilm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  klikfilm
+//
+//  Created by Rangga Leo on 06/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
